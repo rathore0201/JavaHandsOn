a6463c59a341f0adc1d833b5a9fcdf7c0e83869f
rathore0201 <pankaj.rathore@appdirect.com>
2025-07-23 19:15:08 +0530

    multithreading Main

---
+++ src/main/java/org/example/multithreading/Main.java
@@ -0,0 +1,64 @@
+package org.example.multithreading;
+
+public class Main {
+    public static void main(String[] args) {
+//                Hi t1 = new Hi();
+//        Hello t2 = new Hello();
+//
+//        t1.start();
+//        t2.start();
+
+//        SynchronizeCounter counter = new SynchronizeCounter();
+//
+//        Thread t1 = new Thread(() -> {
+//            for (int i=0; i<1000; i++){
+//                counter.increment();
+//            }
+//        });
+//
+//        Thread t2 = new Thread(() -> {
+//            for (int i=0; i<1000; i++){
+//                counter.increment();
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+//        t1.join();
+//        t2.join();
+//
+//        System.out.println("Count :" + counter.count);
+
+//        --------********* interrupt() and yield() ********---------
+//        InterruptAndYield t1 = new InterruptAndYield();
+//        InterruptAndYield t2 = new InterruptAndYield();
+//        t1.start();
+//        t2.start();
+
+
+//        --------********* Explicit Locking **********----------
+//        ExplicitLocking obj = new ExplicitLocking();
+//
+//        Thread t1 = new Thread(() -> {
+//            obj.withDraw(50);
+//        });
+//
+//        Thread t2 = new Thread(() -> {
+//            obj.withDraw(50);
+//        });
+//
+//        t1.start();
+//        t2.start();
+//
+//        t1.join();
+//        t2.join();
+//
+//        System.out.println("Transaction completed.....");
+
+
+//        --------******* ReentrantLock ********----------
+
+//        ReentrantLockExample lockExample = new ReentrantLockExample();
+//        lockExample.outerMethod();
+    }
+}
