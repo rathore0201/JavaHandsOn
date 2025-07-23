371482e5d46103ef5a9632fbf3352df5b6a47fb6
rathore0201 <pankaj.rathore@appdirect.com>
2025-07-23 19:14:44 +0530

    dsa Main

---
+++ src/main/java/org/example/dsa/tree/Main.java
@@ -0,0 +1,11 @@
+package org.example.dsa.tree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(12);
+        tree.insert(10);
+        tree.printTree();
+    }
+}
